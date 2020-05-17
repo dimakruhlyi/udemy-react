bcaa4faf47f2f93f1ae461f5d93ca5f01376172c
dimakruhlyi <dimakruhly@gmail.com>
2020-05-17 23:25:47 +0300

    udemy react-theory first commit to this repo

---
+++ react-theory/src/Car/Car.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import Radium from 'radium';
+import './Car.css';
+
+class Car extends Component{
+    
+    
+    render(){
+        const inputClasses = ['input']
+    
+        if(this.props.name !== ''){
+            inputClasses.push('green')
+        }else{
+            inputClasses.push('red')
+        }
+        if(this.props.name.length > 4){
+            inputClasses.push('bold')
+        }
+    
+        const style = {
+            border: '1px solid #ccc',
+            boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
+            ':hover':{
+                border: '1px solid #aaa',
+                boxShadow: '0 4px 15px 0 rgba(0,0,0, .25)',
+                cursor: 'pointer'
+            }
+        }
+    
+        return(
+            <div className = 'Car' style = {style}>
+                <h3>Car name: {this.props.name}</h3>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input  
+                    type = "text" 
+                    onChange = {this.props.onChangeName} 
+                    value = {this.props.name}
+                    className = {inputClasses.join(' ')}
+                />
+                <button onClick = {this.props.onDelete}>Delete</button>
+            </div>
+        )
+    }
+}
+
+export default Radium(Car)
