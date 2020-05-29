50f6416a44f43bbe5bd7aacfce12b58188a13389
dimakruhlyi <dimakruhly@gmail.com>
2020-05-29 22:56:36 +0300

    first commit

---
+++ react-quiz/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout'
+
+
+function App() {
+  return (
+    <Layout>
+      <div style = {{width: '400px', border: '1px solid black'}}>
+          <h1>Layout works</h1>
+        </div>
+    </Layout>
+  );
+}
+
+export default App;
