a5a96e84ead9c02986910190f062a3655ee95cef
dimakruhlyi <dimakruhly@gmail.com>
2020-06-04 22:22:33 +0300

    react-routing

---
+++ react-routing/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {Route} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </nav>
+
+        <hr/>
+        <Route path = '/' exact render = {() => <h1>Home page</h1>}/>
+        <Route path = '/about' render = {() => <About />}/>
+        
+
+        <Cars />
+      </div>
+    );
+  }
+}
+
+export default App
