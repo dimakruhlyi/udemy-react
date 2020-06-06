c740f8f164d8293484db4a53f1d5fa0a1f2bf338
dimakruhlyi <dimakruhly@gmail.com>
2020-06-07 01:02:01 +0300

    started adding authorization page

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
 
 export default class Auth extends React.Component {
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
     render(){
         return(
-            <div>
-                <h1>Auth page</h1>
+            <div className = {classes.Auth}>
+                <div>
+                    <h1>Authorization</h1>
+
+                    <form onSubmit = {this.submitHandler} className = {classes.AuthForm}>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <Button type = "success" onClick = {this.loginHandler}>Log in</Button>
+                        <Button type = "primary" onClick = {this.registerHandler}>Register</Button>
+                    </form>
+                </div>
             </div>
         )
     }
