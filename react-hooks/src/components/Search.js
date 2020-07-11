545802ce812fc268d2c60ec78e4c2d3ab120be25
dimakruhlyi <dimakruhly@gmail.com>
2020-07-12 01:37:12 +0300

    updated search component

--- react-hooks/src/components/Search.js
+++ react-hooks/src/components/Search.js
@@ -1,16 +1,25 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { AlertContext } from '../context/alert/alertContext';
 
 export const Search = () => {
+    const  [value, setValue] = useState('');
     const {show} = useContext(AlertContext);
     const onSubmit = event => {
-        if(event.key === 'Enter'){
-            show('This is alert!');
+        if(event.key !== 'Enter'){
+            return 
+        }
+
+        if(value.trim()){
+            console.log('Make request with', value);
+        }else{
+            show('Input user\'s data!')
         }
     }
     return(
         <div className="form-group">
-            <input type="text" className = "form-control" placeholder = "Input user's nickname..." onKeyPress = {onSubmit}/>
+            <input type="text" className = "form-control" placeholder = "Input user's nickname..." 
+                onKeyPress = {onSubmit} value = {value} onChange = {event => setValue(event.target.value)}
+            />
         </div>
     )
 }
