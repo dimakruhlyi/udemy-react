4862ab2677d009c795d995df4515be2568799dd8
dimakruhlyi <dimakruhly@gmail.com>
2020-07-11 17:54:54 +0300

    created react-hooks project and added navigation links

---
+++ react-hooks/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+       <div className = 'container pt-4'>
+          <Home />
+      </div>
+    </React.Fragment>
+  
+  );
+}
+
+export default App;
